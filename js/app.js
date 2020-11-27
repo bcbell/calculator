19634aad4b2431363daee385e7d2cb4c0b74b19e
bcbell <coleman.britt@gmail.com>
2020-11-27 00:19:02 -0500

    Update: Final

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-//Produce a result in the display using a number button and number/or result and operator
+//Produce a result in the display using the number buttons and number/or result and operator
 
 function pushBtn(equal){
   document.calculator.display.value = document.calculator.display.value + equal
 }
+
