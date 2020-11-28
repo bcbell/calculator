a974fe78c7fe10f6569f8ef4bf940f2b8ebcab80
bcbell <coleman.britt@gmail.com>
2020-11-28 10:54:52 -0500

    Update: Notes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-//Produce a result in the display using the number buttons and number/or result and operator
+//Display the result of number equation in the display
 
-function pushBtn(equal){
-  document.calculator.display.value = document.calculator.display.value + equal
+function pushBtn(equal){ //the object-funciton has a property with the name "pushBtn" and a parameter of equal 
+   document.calculator.display.value = document.calculator.display.value + equal //the code will look through the documents for a line with the name calculator and then find another line under calculator entitled display and that value  will equal the display value + another value. The equal value will  evaluate the two or more numbers displaying on the screen
 }
 
