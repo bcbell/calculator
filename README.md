07d001df77ccc015b556a64ff7700034d5f76f0b
bcbell <coleman.britt@gmail.com>
2021-01-26 15:05:14 -0500

    Updated

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# calculator
+__Calculator__
+
+Need a quick calculation with an 80s vibe? 
+
+__DEMO__ 
+To demo the calculator : http://lfcalculator.surge.sh/
+
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //Display the result of number equation in the display
 
-function pushBtn(equal){ //the object-funciton has a property with the name "pushBtn" and a parameter of equal 
+function pushBtn(equal){ //the object-function has a property with the name "pushBtn" and a parameter of equal 
    document.calculator.display.value = document.calculator.display.value + equal //the code will look through the documents for a line with the name calculator and then find another line under calculator entitled display and that value  will equal the display value + another value. The equal value will  evaluate the two or more numbers displaying on the screen
 }
 
